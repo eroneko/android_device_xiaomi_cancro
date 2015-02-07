4dcfe7cd368557c58e7667c9093a1a68fa105fff
eroneko <coop.blue.atlas@gmail.com>
2015-02-07 21:19:27 +0900

    Change to PA

--- pa.mk
+++ pa.mk
@@ -23,16 +23,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/pa/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_cancro
+PRODUCT_NAME := pa_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := xiaomi
-PRODUCT_MODEL := MI 3W
+PRODUCT_MODEL := MI 4C
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
